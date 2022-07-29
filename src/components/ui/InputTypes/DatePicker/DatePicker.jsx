ef8a8e6c69142bf350c745946cda232bd3a1e259
0x <chikacheezy@gmail.com>
2022-07-29 08:50:28 +0100

    more on date

--- src/components/ui/InputTypes/DatePicker/DatePicker.jsx
+++ src/components/ui/InputTypes/DatePicker/DatePicker.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-const CustomDatePicker = ({onChange}) => {
+const CustomDatePicker = ({ onChange }) => {
     return (
-        <>
-            <DatePicker onChange={onChange}/>
-        </>
+        <div className={`form-group`}>
+            <DatePicker
+                onChange={onChange}
+                className="form-control"
+                placeholderText="Choose a date"
+                popperPlacement="top-start"
+                portalId="root"
+            />
+        </div>
     );
 };
 
