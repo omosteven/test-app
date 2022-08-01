d7de3a5dff46f57b6c68adb3b95736fbe35cb104
0x <chikacheezy@gmail.com>
2022-08-01 10:36:21 +0100

    update datepicker

--- src/components/ui/InputTypes/DatePicker/DatePicker.jsx
+++ src/components/ui/InputTypes/DatePicker/DatePicker.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import moment from 'moment';
+import React, { useState } from 'react';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 const CustomDatePicker = ({ onChange }) => {
+    const [selectedDate, setSelectedDate] = useState()
     return (
         <div className={`form-group`}>
             <DatePicker
-                onChange={onChange}
+                onChange={(date) => {
+                    setSelectedDate(date)
+                    let dateString = moment(date).format('L');
+                    onChange(dateString)
+                }}
                 className="form-control"
                 placeholderText="Choose a date"
                 popperPlacement="top-start"
                 portalId="root"
+                selected={selectedDate}
             />
         </div>
     );
