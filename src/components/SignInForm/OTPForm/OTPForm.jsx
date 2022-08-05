d03911f6d439fc85c1be40bb06cd06f06ae2f841
mohamilr <ibrahimdamy7@gmail.com>
2022-08-05 08:18:47 +0100

    fix page reload

--- src/components/SignInForm/OTPForm/OTPForm.jsx
+++ src/components/SignInForm/OTPForm/OTPForm.jsx
@@ -33,9 +33,10 @@ const OTPForm = ({ initialStepRequest }) => {
             });
             if (res.status === 200) {
                 const { data } = res.data;
-                pushToDashboard(data, () =>
-                    history.replace(`/chat?workspaceSlug=${workspaceSlug}`)
-                );
+                pushToDashboard(data, () => {
+                    history.replace(`/chat?workspaceSlug=${workspaceSlug}`);
+                    window.location.reload();
+                });
             }
         } catch (err) {
             setErrorMsg(getErrorMessage(err));
