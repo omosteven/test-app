1928ac90cf1a5279b560d18cc519b5de79a7cc84
mohamilr <ibrahimdamy7@gmail.com>
2022-08-15 10:09:06 +0100

    fix useEffect dependency

--- src/components/Chat/ChatModule/LiveChat/LiveChat.js
+++ src/components/Chat/ChatModule/LiveChat/LiveChat.js
@@ -8,7 +8,6 @@ import {
     SEND_BRANCH_OPTION,
     SEND_CUSTOMER_CONVERSATION_REPLY,
     SEND_CUSTOMER_MESSAGE,
-    NEW_TEST_TICKET,
 } from "../../../../lib/socket/events";
 import { dataQueryStatus } from "../../../../utils/formatHandlers";
 import { generateID, getErrorMessage } from "../../../../utils/helper";
@@ -507,7 +506,7 @@ const LiveChat = ({ getCustomerTickets }) => {
         figureInputAction();
         processIssueDiscovery();
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [messages]);
+    }, [ticketsMessages]);
 
     return (
         <>
