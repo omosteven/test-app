8320d0b88e85c7883fa90f07c5842674c6b21373
omosteven <omosteven123@gmail.com>
2022-08-15 08:57:25 +0100

    updated public

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,4 +1,4 @@
-import { firebaseConfig } from "../src/lib/firebase/config/config";
+// import { firebaseConfig } from "../src/lib/firebase/config/config";
 
 // Scripts for firebase and firebase messaging
 importScripts(
@@ -7,6 +7,16 @@ importScripts(
 importScripts(
     "https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js"
 );
+
+firebaseConfig = {
+    apiKey: "AIzaSyA3gJMtC8_nqyyMGG1k4Xjsou0uWYOJ2NQ",
+    authDomain: "metacare-610d3.firebaseapp.com",
+    projectId: "metacare-610d3",
+    storageBucket: "metacare-610d3.appspot.com",
+    messagingSenderId: "354799129472",
+    appId: "1:354799129472:web:ff25b9163856a3e31ce765",
+    measurementId: "G-V65CYV6YWM",
+};
 
 firebase.initializeApp(firebaseConfig);
 
