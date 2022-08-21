23b949b40e88a9eaa2752e4d7759af031403b8c3
mohamilr <ibrahimdamy7@gmail.com>
2022-08-21 01:58:29 +0100

    fix change option for suggestions

--- src/components/Chat/ChatModule/TicketsHeader/ChangeOption/ChangeOption.jsx
+++ src/components/Chat/ChatModule/TicketsHeader/ChangeOption/ChangeOption.jsx
@@ -1,68 +1,88 @@
-import React from 'react';
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
-import API from '../../../../../lib/api';
-import apiRoutes from '../../../../../lib/api/apiRoutes';
-import { dataQueryStatus } from '../../../../../utils/formatHandlers';
-import { getErrorMessage } from '../../../../../utils/helper';
-import { Button } from '../../../../ui';
-import { appMessageUserTypes, messageTypes } from '../../LiveChat/MessageBody/Messages/Message/enums';
+import React from "react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import API from "../../../../../lib/api";
+import apiRoutes from "../../../../../lib/api/apiRoutes";
+import { dataQueryStatus } from "../../../../../utils/formatHandlers";
+import { getErrorMessage } from "../../../../../utils/helper";
+import { Button } from "../../../../ui";
+import {
+    appMessageUserTypes,
+    messageTypes,
+} from "../../LiveChat/MessageBody/Messages/Message/enums";
 
 const { LOADING } = dataQueryStatus;
-const ChangeOption = ({  setStatus, setActiveConvo, setErrorMssg, requestAllMessages }) => {
+const ChangeOption = ({
+    setStatus,
+    setActiveConvo,
+    setErrorMssg,
+    requestAllMessages,
+}) => {
     const [loading, setLoading] = useState(false);
-    const { chatSettings: { chatThemeColor }} = useSelector(state => state.chat)
-    const { activeTicket:ticket, ticketsMessages } = useSelector(state => state.tickets)
+    const {
+        chatSettings: { chatThemeColor },
+    } = useSelector((state) => state.chat);
+    const { activeTicket: ticket, ticketsMessages } = useSelector(
+        (state) => state.tickets
+    );
     const { ticketId } = ticket;
-    const messages = ticketsMessages?.filter((item) => item?.ticketId === ticketId);
-
+    const messages = ticketsMessages?.filter(
+        (item) => item?.ticketId === ticketId
+    );
 
     const changeLastBranchOptionChoice = async () => {
         try {
-            setLoading(true)
+            setLoading(true);
             setStatus(LOADING);
             setErrorMssg();
 
             let allMessagesCopy = [...messages];
-            let lastAgentMssg = [...allMessagesCopy].reverse()?.find(message => message.senderType === appMessageUserTypes?.WORKSPACE_AGENT);
+            let lastAgentMssg = [...allMessagesCopy]
+                .reverse()
+                ?.find(
+                    (message) =>
+                        message.senderType ===
+                        appMessageUserTypes?.WORKSPACE_AGENT
+                );
 
-            if(lastAgentMssg?.messageType === messageTypes.CONVERSATION){
+            if (
+                lastAgentMssg?.messageType === messageTypes.CONVERSATION ||
+                lastAgentMssg?.messageType === messageTypes.BRANCH
+            ) {
                 // console.log("Pause there")
-                setActiveConvo(false)
+                setActiveConvo(false);
                 // return ""
             }
 
             const url = apiRoutes?.changeTicketChoice(ticketId);
             const res = await API.post(url);
             if (res.status === 201) {
-                requestAllMessages()
-                setLoading(false)
+                requestAllMessages();
+                setLoading(false);
             }
-
         } catch (err) {
-            setLoading(false)
+            setLoading(false);
             setStatus(dataQueryStatus?.ERROR);
             setErrorMssg(getErrorMessage(err));
         }
-    }
+    };
 
     // chatThemeColor
     return (
-
         <div>
-            <Button 
-                text={'Change Choice'}
-                classType={'change-choice'}
+            <Button
+                text={"Change Choice"}
+                classType={"change-choice"}
                 onClick={(e) => {
                     e.preventDefault();
-                    changeLastBranchOptionChoice()
+                    changeLastBranchOptionChoice();
                 }}
                 disabled={loading}
                 style={{
                     color: chatThemeColor,
-                    background: `${chatThemeColor}21`
+                    background: `${chatThemeColor}21`,
                 }}
-             />
+            />
         </div>
     );
 };
