945de5cd6cc595a35c1c5c0c545fd3846f1264e2
mohamilr <ibrahimdamy7@gmail.com>
2022-09-11 20:01:52 +0100

    use color variables

--- src/components/Chat/CustomerTicketsContainer/CustomerTickets/common/NewTicketButton/NewTicketButton.js
+++ src/components/Chat/CustomerTicketsContainer/CustomerTickets/common/NewTicketButton/NewTicketButton.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { ReactSVG } from 'react-svg';
-import imageLinks from '../../../../../../assets/images';
-import { Button } from '../../../../../ui';
+import React from "react";
+import { ReactSVG } from "react-svg";
+import imageLinks from "assets/images";
+import { Button } from "components/ui";
 
-const NewTicketButton = ({ handleClick, otherClassNames}) => {
+const NewTicketButton = ({ handleClick, otherClassNames }) => {
     return (
         <Button
-            icon={<ReactSVG src={imageLinks.svg.add} className="icon" />}
-            text="New conversation"
+            icon={<ReactSVG src={imageLinks.svg.add} className='icon' />}
+            text='New conversation'
             otherClass={`new__convo ${otherClassNames ? otherClassNames : ""}`}
             onClick={handleClick}
         />
