1e056075c75bf19f675b070decfa443b9b65bc85
mohamilr <ibrahimdamy7@gmail.com>
2023-03-30 18:27:39 +0100

    remove test ellipsis

--- src/components/Chat/ChatModule/ChatHeader/ChatHeader.js
+++ src/components/Chat/ChatModule/ChatHeader/ChatHeader.js
@@ -180,11 +180,6 @@ const ChatHeader = ({
                             )}
                         </>
                     )}
-                    <ChatSettingsToggler
-                        isMobile={true}
-                        handleCloseTicket={handleCloseTicket}
-                        canCloseTicket={ticketId !== undefined}
-                    />
                     {canSaveConvo && (
                         <SaveChatButton
                             handleVerifyAction={handleVerifyAction}
