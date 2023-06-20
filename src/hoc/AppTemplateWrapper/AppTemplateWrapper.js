31ce9d6403297ba8e5127914cb2f96982748b64d
omosteven <omosteven123@gmail.com>
2023-06-20 18:14:38 +0100

    started adjustments for ios ui

--- src/hoc/AppTemplateWrapper/AppTemplateWrapper.js
+++ src/hoc/AppTemplateWrapper/AppTemplateWrapper.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { defaultThemes, defaultTemplates } from "./enum";
 import "./AppTemplateWrapper.scss";
+import { isIOSDevice } from "utils/helper";
 
 const { WHITE_MODE_DEFAULT, DARK_MODE_DEFAULT } = defaultThemes;
 const { WORKMODE, RELAXED } = defaultTemplates;
@@ -16,6 +17,8 @@ const AppTemplateWrapper = ({ children }) => {
     const isDarkModeTheme = defaultTheme === DARK_MODE_DEFAULT;
     const isWhiteModeTheme = defaultTheme === WHITE_MODE_DEFAULT;
 
+    const isIOS = isIOSDevice();
+
     return (
         <div
             className={`${
@@ -28,7 +31,9 @@ const AppTemplateWrapper = ({ children }) => {
                         ? "white-mode-default"
                         : "dark-mode-default"
                     : ""
-            } ${isWorkModeTemplate ? "work-mode" : "relaxed-mode"}`}>
+            } ${isWorkModeTemplate ? "work-mode" : "relaxed-mode"} ${
+                isIOS ? "ios__device__style" : ""
+            }`}>
             {children}
         </div>
     );
